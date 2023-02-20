676ed1458d03df8491364c9d1db65c55f0d3f8b7
ibra <amerhanov_ibragim95@mail.ru>
2023-02-20 22:15:09 +0300

    firstCommit

--- src/Author.java
+++ src/Author.java
@@ -1,5 +1,26 @@
 public class Author {
-    String name = "Ivan";
-    String surname ="Ivanov";
+   private String name;
+   private String surname;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Author(String name,String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
 
 }
